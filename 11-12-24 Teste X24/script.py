ff0ce5ba4982d0929e580043a1f4124418687652
Lucas Ribeiro <145038823+lribeiroalves@users.noreply.github.com>
2024-12-18 05:50:27 -0300

    Update script.py

--- 11-12-24 Teste X24/script.py
+++ 11-12-24 Teste X24/script.py
@@ -7,7 +7,9 @@ Created on Tue Dec 17 12:44:19 2024
 
 #%% Bibliotecas
 
-
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 #%% Preparação dos dados para importação
 
@@ -61,3 +63,54 @@ with open('dados.csv', 'w') as f:
             else:
                 f.write('\n')
                 break
+            
+#%% Importação dos Dados
+
+dados_brutos = pd.read_csv('dados.csv')
+dados_brutos.info()
+
+#%% Calculo da temperatura
+
+dados_tratados = dados_brutos.assign(temp=dados_brutos['temp'] / 5)
+
+#%% Calculo do RSSI
+
+dados_tratados = dados_tratados.assign(rssi = lambda x: -1 * (256 - x.rssi))
+
+#%% filtro de valores incorretos
+
+dados_tratados = dados_tratados[dados_tratados['day'] <= 31]
+dados_tratados = dados_tratados[dados_tratados['month'] <= 12]
+dados_tratados = dados_tratados[dados_tratados['year'] == 2024]
+dados_tratados = dados_tratados[dados_tratados['hour'] <= 24]
+dados_tratados = dados_tratados[dados_tratados['minute'] <= 59]
+dados_tratados = dados_tratados[dados_tratados['second'] <= 59]
+dados_tratados = dados_tratados[dados_tratados['gX'] > 0]
+dados_tratados = dados_tratados[dados_tratados['gY'] > 0]
+dados_tratados = dados_tratados[dados_tratados['gZ'] > 0]
+
+#%% Criação de coluna de Data
+
+# Correção do ano
+dados_tratados = dados_tratados.assign(year = dados_tratados.year + 2000)
+
+# Coluna de Data
+dados_tratados = dados_tratados.assign(date = pd.to_datetime(dados_tratados[['year', 'month', 'day', 'hour', 'minute', 'second']]))
+
+dados_tratados[['year', 'month', 'day', 'hour', 'minute', 'second']].describe()
+
+#%% Selecionar dados uteis
+
+dados_uteis = dados_tratados[['date', 'temp', 'umid', 'gX', 'gY', 'gZ', 'rssi']]
+
+dados_uteis[['gX', 'gY', 'gZ']].describe()
+
+#%% Exibir resultados
+
+plt.figure(figsize=(15,9), dpi=3000)
+# sns.lineplot(data=dados_uteis, x='date', y='umid')
+# sns.lineplot(data=dados_uteis, x='date', y='temp')
+sns.lineplot(data=dados_uteis, x='date', y='gX')
+sns.lineplot(data=dados_uteis, x='date', y='gY')
+sns.lineplot(data=dados_uteis, x='date', y='gZ')
+plt.show()
