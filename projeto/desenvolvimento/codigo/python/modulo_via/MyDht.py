89fef5a9054302790e91e528af18680217dd1a96
Lucas Ribeiro <145038823+lribeiroalves@users.noreply.github.com>
2025-01-02 02:34:20 -0300

    myDht class implemented

---
+++ projeto/desenvolvimento/codigo/python/modulo_via/MyDht.py
@@ -0,0 +1,71 @@
+from machine import Pin
+import dht as DHT
+from time import sleep, sleep_ms, ticks_ms
+
+
+class MyDht:
+    """ Classe para implementação específica do uso do modulo dht
+    para leituras de umidade e temperatura em períodos predefinidos"""
+    def __init__(self, pin, time_ms=1000):
+        self.__dht_object = DHT.DHT11(Pin(pin))
+        self.__read_time = time_ms
+        self.__previous_time = 0
+        self.__readings = [0, 0]
+        self.__update_enable = True
+    
+    
+    @property
+    def readings(self):
+        return self.__readings
+    
+    
+    @property
+    def update_enable(self):
+        return self.__update_enable
+    
+    
+    @update_enable.setter
+    def update_enable(self, flag: bool):
+        if flag == True:
+            self.__update_enable = True
+            self.__readings = [0, 0]
+        else:
+            raise ValueError('Esse atributo só pode ser alterado para verdadeiro.')
+    
+    
+    def read_dht(self) -> tuple[int, int]:
+        """ Realiza uma leitura do sensor e retorna uma tupla (temperatura * 5, umidade),
+            em caso de erro, repete recursivamente até que consiga o valor """
+        try:
+            self.__dht_object.measure()
+            return (self.__dht_object.temperature() * 5, self.__dht_object.humidity())
+        except OSError:
+            sleep_ms(10)
+            return self.read_dht()
+        except Exception as err:
+            print(err)
+            return (255, 255)
+    
+    
+    def update(self):
+        """ Solicita ao sensor os valores repetidamente após o período predeterminado
+            e atualiza a lista de retorno """
+        current_time = ticks_ms()
+        
+        if current_time >= self.__previous_time + self.__read_time and self.__update_enable == True:
+            self.__previous_time = current_time
+            # Realiza a leitura a salva na lista de retorno, desabilita novas leituras
+            r = self.read_dht()
+            self.__readings = [r[0], r[1]]
+            self.__update_enable = False
+
+
+if __name__ == '__main__':
+    # Exemplo de aplicação da classe MyDht
+    dht = MyDht(pin=33)
+    while True:
+        dht.update()
+        if not dht.update_enable:
+            print(dht.readings)
+            dht.update_enable = True
+        
