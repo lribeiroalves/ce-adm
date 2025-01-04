d7286fc29322e4cc5be60053e00b834fc3b2e773
Lucas Ribeiro <145038823+lribeiroalves@users.noreply.github.com>
2025-01-04 09:42:39 -0300

    Update i2c_scan.py

--- apoio/MicroPython/exemplos/i2c_scan.py
+++ apoio/MicroPython/exemplos/i2c_scan.py
@@ -1,13 +1,18 @@
 from machine import Pin, SoftI2C
-import time
-from ssd1306 import SSD1306_I2C
+
+sda_pin = 21
+scl_pin = 22
+list_rst_pin = None # must be a list of rst pins of the devices
 
 # Configuração do I2C
-i2c = SoftI2C(scl=Pin(15), sda=Pin(4))
+i2c = SoftI2C(scl=Pin(scl_pin), sda=Pin(sda_pin))
 
-pin = Pin(16, Pin.OUT)
-pin.value(0) #set GPIO16 low to reset OLED
-pin.value(1) #while OLED is running, must set GPIO16 in high
+# Alguns dispositivos I2C precisam de um reset via software antes de iniciar a comunicação
+if list_rst_pin:
+    for pin in list_rst_pin:
+        pino = Pin(pin, Pin.OUT)
+        pino.value(0)
+        pino.value(1)
 
 # Escanear dispositivos I2C no barramento
 dispositivos = i2c.scan()
