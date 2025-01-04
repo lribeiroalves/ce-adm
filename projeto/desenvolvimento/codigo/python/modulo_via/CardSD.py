154dab4d5488e385024c870b6ec83102073783b3
Lucas Ribeiro <145038823+lribeiroalves@users.noreply.github.com>
2025-01-04 16:55:51 -0300

    sdcard implemented

--- projeto/desenvolvimento/codigo/python/modulo_via/CardSD.py
+++ projeto/desenvolvimento/codigo/python/modulo_via/CardSD.py
@@ -1,14 +1,98 @@
 from machine import Pin, SPI
-import sdcard
+from sdcard import SDCard
 import os
 
 
 class CardSD:
     """ Classe para utilização do barramento SPI para comunicação com cartão SD """
     
-    def __init__(self):
-        pass
+    def __init__(self, pin_cs: int = 5, pin_sck: int = 18, pin_mosi: int = 23, pin_miso: int = 19):
+        self.__spi = SPI(1,baudrate=1000000, phase=0, polarity=0, sck=Pin(pin_sck), mosi=Pin(pin_mosi), miso=Pin(pin_miso))
+        self.__sd = SDCard(self.__spi, Pin(pin_cs))
+        os.mount(self.__sd, '/sd')
+    
+    
+    def __dir(self, path):
+        caminho = '/'.join(path.split('/')[:-1])
+        arquivo = path.split('/')[-1]
+        
+        try:
+            diretorio = os.listdir(caminho)
+        except OSError:
+            raise ValueError('Directory not Found')
+        
+        return diretorio, arquivo
+        
+    
+    def list_files(self, path):
+        """ List all files and directories on the path """
+        try:
+            for item in os.listdir(path):
+                full_path = path + '/' + item
+                if os.stat(full_path)[0] & 0x4000:  # Verifica se é um diretório
+                    print(f'DIR: {full_path}')
+                    self.list_files(full_path)  # Lista arquivos no diretório
+                else:
+                    print(f'FILE: {full_path}')
+        except OSError:
+            raise ValueError('Path Fot Found.')
+    
+    
+    def write_data(self, path: str, data: str, mode: str = 'w'):
+        """ Escrever no cartão SD - 'mode' pode assumir 'A' ou 'W' para concatenar ao arquivo ou limpar e escrever """
+        if mode.lower() in ['a', 'w']:
+            # Criar diretório se não existir
+            dir_path = '/'.join(path.split('/')[:-1])
+            try:
+                os.listdir(dir_path)
+            except OSError:
+                os.mkdir(dir_path)
+            # Escrever dados no arquivo
+            with open(path, mode.lower()) as file:
+                file.write(data)
+        else:
+            raise ValueError('mode must be "a" or "w"')
+    
+    
+    def read_data(self, full_path):
+        """ Tenta ler os dados de um arquivo e retorna um ValueError se nao encontrar o arquivo. """
+        diretorio, arquivo = self.__dir(full_path)
+        if arquivo in diretorio:
+            with open(full_path, 'r') as file:
+                print(file.read())
+        else:
+            raise ValueError('File Not Found.')
+    
+    
+    def clear_file(self, full_path):
+        """ Limpa os dados de um arquivo """
+        diretorio, arquivo = self.__dir(full_path)
+        if arquivo in diretorio:
+            self.write_data(full_path, '', 'w')
+        else:
+            raise ValueError('File Not Found.')
+    
+    
+    def delete_from_card(self, full_path):
+        """ Delete um arquivo ou diretorio do Cartão SD """
+        diretorio, arquivo = self.__dir(full_path)
+        if arquivo in diretorio:
+            if os.stat(full_path)[0] & 0X4000:
+                for file in os.listdir(full_path):
+                    file_path = full_path + '/' + file
+                    if os.stat(full_path)[0] & 0X4000:
+                        self.delete_from_card(file_path)
+                    else:
+                        os.remove(file_path)
+                os.rmdir(full_path)
+            else:
+                os.remove(full_path)
+        else:
+            raise ValueError('File Not Found')
 
 
 if __name__ == '__main__':
-    pass
+    sd = CardSD()
+    sd.list_files('/sd')
+    sd.read_data('/sd/data_logger.txt')
+    
--- projeto/desenvolvimento/codigo/python/modulo_via/main.py
+++ projeto/desenvolvimento/codigo/python/modulo_via/main.py
@@ -1,22 +1,35 @@
 from MyDht import MyDht
 from Gyroscope import Gyroscope
 from Clock import Clock
+from CardSD import CardSD
 
 dht = MyDht()
 gyro = Gyroscope()
 clock = Clock()
+sd = CardSD()
 
+# Definição do horário inicial do RTC interno
 clock.set_time(25, 1, 3, 18, 23, 40)
+
+# Criação do arquivo data_logger.txt que armazenará as informações de leituras
+dte = clock.get_time()
+logger_path = f'/sd/data_logger/{dte["ano"]}_{dte["mes"]}_{dte["dia"]}.csv'
+sd.write_data(logger_path, 'day,month,year,hour,minute,second,umid,temp,gX,gY,gZ\n', 'w')
 
 
 def make_pack():
     dte = clock.get_time()
-    p = {'temperatura': dht.readings[0], 'umidade': dht.readings[1],
+    p = {'temp': dht.readings[0], 'umid': dht.readings[1],
               'gX': gyro.readings[0], 'gY': gyro.readings[1], 'gZ': gyro.readings[2],
-              'ano': dte['ano'] - 2000, 'mes': dte['mes'], 'dia': dte['dia'],
-              'hora': dte['hora'], 'minuto': dte['minuto'], 'segundo': dte['segundo']
+              'year': dte['ano'] - 2000, 'month': dte['mes'], 'day': dte['dia'],
+              'hour': dte['hora'], 'minute': dte['minuto'], 'second': dte['segundo']
          }
     return p
+
+
+def make_datalog(pacote):
+    return f"{pacote['day']},{pacote['month']},{pacote['year']},{pacote['hour']},{pacote['minute']},{pacote['second']},{pacote['umid']},{pacote['temp']},{pacote['gX']},{pacote['gY']},{pacote['gZ']}\n"
+
 
 while True:
     dht.update()
@@ -25,5 +38,7 @@ while True:
     if not dht.update_enable and not gyro.update_enable:
         pacote = make_pack()
         print(pacote)
+        datalog = make_datalog(pacote)
+        sd.write_data(logger_path, datalog, 'a')
         dht.update_enable = True
         gyro.update_enable = True
