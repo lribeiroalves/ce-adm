4e7a9b9cbc6868edbb4181a85c2f7aec7d57e58d
Lucas Ribeiro <145038823+lribeiroalves@users.noreply.github.com>
2025-01-04 10:05:46 -0300

    Create CardSD.py

---
+++ projeto/desenvolvimento/codigo/python/modulo_via/CardSD.py
@@ -0,0 +1,14 @@
+from machine import Pin, SPI
+import sdcard
+import os
+
+
+class CardSD:
+    """ Classe para utilização do barramento SPI para comunicação com cartão SD """
+    
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
