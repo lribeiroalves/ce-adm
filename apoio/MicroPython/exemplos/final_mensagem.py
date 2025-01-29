9cc9ba20f102df7847e42b08170c6e79dc8e890d
LUCAS RIBEIRO ALVES <lucas.alves@metrosp.com.br>
2025-01-29 04:00:23 -0300

    final_mensagem

---
+++ apoio/MicroPython/exemplos/final_mensagem.py
@@ -0,0 +1,19 @@
+def verificar_final_de_mensagem(buffer:list):
+    mensagens_separadas = []
+    final = [0xFF, 0xFF, 0xFF]
+    final_len = len(final)
+    buffer_len = len(buffer)
+    
+    if buffer_len >= final_len + 5:
+        for i in range(buffer_len - final_len + 1):
+            if buffer[i:i + final_len] == final:
+                mensagem = buffer[i - buffer[i + final_len]: i + final_len + 4]
+                mensagens_separadas.append(mensagem)
+    else:
+        return False
+
+if __name__ == '__main__':
+    m1 = [0x01, 0x03, 0x10, 25, 1, 25, 0, 0, 0, 85, 125, 0, 0, 0, 5, 0, 3, 30, 0xFF, 0xFF, 0xFF, 18, 0x00, 0x00, 0x1]
+    print(len(m1))
+    
+    
