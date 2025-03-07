3d14bce20a7c02a116c5ff03cd18f6e93c53ec6e
lribeiroalves <lribeiroalves@users.noreply.github.com>
2025-03-07 04:18:01 -0300

    new flask update

--- apoio/serv_melhorado/adm_server/ext/database/models.py
+++ apoio/serv_melhorado/adm_server/ext/database/models.py
@@ -24,7 +24,7 @@ class BaseModel():
     """ Modelo base para todas as tabelas """
     id:Mapped[int] = mapped_column(primary_key=True)
     num_pacote:Mapped[int] = mapped_column(Integer(), nullable=False, unique=False)
-    date:Mapped[DateTime] = mapped_column(DateTime(timezone=True), nullable=False, unique=False)
+    date:Mapped[DateTime] = mapped_column(DateTime(timezone=True, fsp=3), nullable=False, unique=False)
 
     def __repr__(self):
         return f'{self.__class__.__name__}(id = {self.id, self.num_pacote}'
